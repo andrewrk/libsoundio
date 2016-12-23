8af2379cf7bbc23bce318dadff1035e0ad6f3eb2
Anthony Pesch <inolen@gmail.com>
2016-12-22 19:43:14 -0800

    build: fix detection of unsuitable JACK library

--- cmake/FindJACK.cmake
+++ cmake/FindJACK.cmake
@@ -10,7 +10,10 @@ find_path(JACK_INCLUDE_DIR NAMES jack/jack.h)
 
 find_library(JACK_LIBRARY NAMES jack)
 
+include(CheckLibraryExists)
+check_library_exists(jack "jack_set_port_rename_callback" "${JACK_LIBRARY}" HAVE_jack_set_port_rename_callback)
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JACK DEFAULT_MSG JACK_LIBRARY JACK_INCLUDE_DIR)
+find_package_handle_standard_args(JACK DEFAULT_MSG JACK_LIBRARY JACK_INCLUDE_DIR HAVE_jack_set_port_rename_callback)
 
 mark_as_advanced(JACK_INCLUDE_DIR JACK_LIBRARY)
