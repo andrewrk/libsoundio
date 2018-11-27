aa0df243bbac671dc10cf951856fb23c4ed21673
Antonin Décimo <antonin.decimo@gmail.com>
2018-11-28 00:54:15 +0100

    Fix #193 alsa backend not handling SND_PCM_STATE_PRIVATE1
    This is a private value of alsa, ignore it.

--- src/alsa.c
+++ src/alsa.c
@@ -1191,6 +1191,8 @@ static void outstream_thread_run(void *arg) {
             case SND_PCM_STATE_DISCONNECTED:
                 outstream->error_callback(outstream, SoundIoErrorStreaming);
                 return;
+            default:
+                continue;
         }
     }
 }
@@ -1260,6 +1262,8 @@ static void instream_thread_run(void *arg) {
             case SND_PCM_STATE_DISCONNECTED:
                 instream->error_callback(instream, SoundIoErrorStreaming);
                 return;
+            default:
+                continue;
         }
     }
 }
