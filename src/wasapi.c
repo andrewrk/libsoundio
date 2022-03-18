d3012d2f2eff4beab68b54f6f9720be0e196f1aa
Mason Remaley <mason.remaley@gmail.com>
2022-03-17 20:56:36 -0400

    Uses the user requested buffer size on wasapi
    Windows appears to automatically adjust the timer period when you
    use a small buffer, meaning that using a fraction of a large buffer
    is unreliable unless you call timeBeginPeriod. Seems simpler to just
    request the desired size directly.
    
    I'll do some further investigations into this, if it seems like the
    right change to make I'll update the docs as well if needed.

--- src/wasapi.c
+++ src/wasapi.c
@@ -1283,7 +1283,7 @@ static int outstream_do_open(struct SoundIoPrivate *si, struct SoundIoOutStreamP
         flags = osw->need_resample ? AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM | AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY : 0;
         share_mode = AUDCLNT_SHAREMODE_SHARED;
         periodicity = 0;
-        buffer_duration = to_reference_time(4.0);
+        buffer_duration = to_reference_time(outstream->software_latency);
     }
     to_wave_format_layout(&outstream->layout, &wave_format);
     to_wave_format_format(outstream->format, &wave_format);
@@ -1820,7 +1820,7 @@ static int instream_do_open(struct SoundIoPrivate *si, struct SoundIoInStreamPri
         flags = 0;
         share_mode = AUDCLNT_SHAREMODE_SHARED;
         periodicity = 0;
-        buffer_duration = to_reference_time(4.0);
+        buffer_duration = to_reference_time(instream->software_latency);
     }
     to_wave_format_layout(&instream->layout, &wave_format);
     to_wave_format_format(instream->format, &wave_format);
